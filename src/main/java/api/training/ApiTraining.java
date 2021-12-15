93f6d4cd1f87b58a125aee42e9e3606ea6c4745b
Roman Lukashuk <RomanLukashuk@coherentsolutions.com>
2021-12-15 21:00:04 +0300

    Updated previous commit: added code in ApiTraining class

--- src/main/java/api/training/ApiTraining.java
+++ src/main/java/api/training/ApiTraining.java
@@ -1,7 +1,13 @@
 package api.training;
 
+import api.training.services.AuthenticationService;
+
 public class ApiTraining {
 
 	public static void main(String[] args) {
+		String readToken = AuthenticationService.getInstance().getToken("read");
+		System.out.println("Token: " + readToken);
+		String writeToken = AuthenticationService.getInstance().getToken("write");
+		System.out.println("Token: " + writeToken);
 	}
 }
