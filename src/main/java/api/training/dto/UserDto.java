fd7efae9c495c27fafbd52f44d294469eeee35bf
Romero10 <r.lukashuk93@gmail.com>
2022-01-07 17:21:08 +0300

    Added equals() and hashCode() methods for UserDto class.

--- src/main/java/api/training/dto/UserDto.java
+++ src/main/java/api/training/dto/UserDto.java
@@ -1,13 +1,17 @@
 package api.training.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 @Getter
 @Setter
-@EqualsAndHashCode
 public class UserDto implements IModel {
 
 	@JsonProperty("age")
@@ -22,7 +26,32 @@ public class UserDto implements IModel {
 	@JsonProperty("zipCode")
 	private String zipCode;
 
-    public String toString() {
-        return getJsonString(UserDto.class);
-    }
+	@JsonIgnore
+	private Map<String, Sex> namePlusSex = new HashMap<>();
+
+	public String toString() {
+		return getJsonString(UserDto.class);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		UserDto userDto = (UserDto) o;
+		return new EqualsBuilder()
+				.append(age, userDto.age)
+				.append(getComplexKey(), userDto.getComplexKey())
+				.append(zipCode, userDto.zipCode)
+				.isEquals();
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(age, name, sex, zipCode);
+	}
+
+	private Map<String, Sex> getComplexKey() {
+		namePlusSex.put(name, sex);
+		return namePlusSex;
+	}
 }
